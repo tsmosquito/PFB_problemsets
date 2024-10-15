65ae78adb778f6eca14d843aeee06516918535ee
pfb2024 <pfb2024@info09.cshl.edu>
2024-10-15 11:35:26 -0400

    adding about me file with name, color, hobby.

---
+++ Python_01_probs/about_me.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+my_first_name = "Tim"
+print("My name:", my_first_name)
+
+my_fav_colors = "gold and green"
+print("My fave colors:", my_fav_colors)
+
+my_obsession = "biiiiikkkkiiiiinnnngggg!!!!!"
+print("My obsession:",my_obsession)
